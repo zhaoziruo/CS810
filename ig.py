9f78a34f92eb41f1242f9105e7d425532efa3243
Ziruo Zhao <153393836+zhaoziruo@users.noreply.github.com>
2025-06-04 02:29:31 -0400

    Add files via upload

---
+++ ig.py
@@ -0,0 +1,53 @@
+# BERT model with Integrated Gradients (IG)
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import torch.nn.functional as F
+from captum.attr import IntegratedGradients
+
+#bert model finetuned on mnli
+#swap Adam optimizer with M-FAC
+model_name = "M-FAC/bert-tiny-finetuned-mnli" 
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+model.eval()
+
+
+# 0:entailment, 1:neutral, 2:contradiction
+target_label = 1
+
+#take an example
+premise = "Your gift is appreciated by each and every student who will benefit from your generosity."
+hypothesis = "Hundreds of students will benefit from your generosity."
+encoded = tokenizer(premise,hypothesis,
+                    return_tensors="pt",
+                    truncation=True,
+                    max_length=128,
+                    padding="max_length")
+input_ids      = encoded["input_ids"]
+attention_mask = encoded["attention_mask"]
+
+# forward
+def forward_func(input_ids, attention_mask):
+    logits = model(input_ids=input_ids, attention_mask=attention_mask).logits
+    return F.softmax(logits, dim=1)
+
+# IntegratedGradients
+ig = IntegratedGradients(forward_func)
+
+# attribution
+attributions, delta = ig.attribute(
+    inputs=input_ids,
+    additional_forward_args=(attention_mask,),
+    target=target_label,
+    return_convergence_delta=True)
+
+# token-level IG value
+tokens = tokenizer.convert_ids_to_tokens(input_ids[0])
+scores = attributions[0].sum(dim=1)
+for tok, score in zip(tokens, scores):
+    print(f"{tok:>12} : {score.item(): .4f}")
+
+print(f"\nConvergence delta: {delta.item():.4e}")
+
